d93712b91e015446eb78a75b2b5ab61d7646d4b9
qq16 <hh3088115843@gmail.com>
2021-10-10 21:14:33 +0800

    Required gulp-sourcemaps in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,26 @@ const gulp = require("gulp");
 const { series } = require("gulp");
 const ts = require("gulp-typescript");
 const tsProject = ts.createProject("tsconfig.json");
+const sourcemaps = require("gulp-sourcemaps");
 
-// `clean` 函数并未被导出（export），因此被认为是私有任务（private task）。
-// clean 和 build 都可以被用在 `series()` 组合中。
 function clean(cb) {
-  // body omitted
   cb();
 }
 
-// `build` 函数被导出（export）了，因此它是一个公开任务（public task），
-// 并且可以被 `gulp` 命令直接调用。
 function build(cb) {
   cb();
   return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest("dist"));
 }
 
+function genSourceMap(cb) {
+  cb();
+  return gulp
+    .src("dist/*.js")
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest("dist"));
+}
+
 exports.build = build;
-exports.default = series(clean, build);
+exports.genSourceMap = genSourceMap;
+exports.default = series(clean, build, genSourceMap);
