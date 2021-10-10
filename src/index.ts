4e5e1d1f1bf9ca3c860934392408340cff53b901
qq16 <1811639092@qq.com>
2021-10-10 17:58:17 +0800

    Added README.md

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
 import 'reflect-metadata';
 
-function logType(target: any, key: string) {
-  let t = Reflect.getMetadata('design:type', target, key);
-  console.log(`${key} type: ${t.name}`);
+function logParamTypes(target: any, key: string) {
+  const t = Reflect.getMetadata('design:paramtypes', target, key);
+  const s = t.map((i:{name:string})=>i.name).join();
+  console.log(`${key} type: ${s}`);
+}
+
+declare interface IFoo {
+
 }
 
 class Demo {
-  @logType
-  private attr: string;
+  @logParamTypes // apply parameter decorator
+    doSomething(
+      param1: string,
+      param2: number,
+      param3: any,
+      param4: { test: string },
+      param5: IFoo,
+      param6: Function,
+      param7: (a: number) => void,
+    ): number {
+      return 1;
+    }
 }
